1452c48174b65bc84bb04ecf59cac6b1f5a72e1e
suheri77 <153284084+suheri77@users.noreply.github.com>
2024-05-16 07:01:54 +0700

    Update app2.js

--- app2.js
+++ app2.js
@@ -1,140 +1,375 @@
-const bars = document.querySelector(".bar"),
-close = document.querySelector(".close"),
-menu = document.querySelector(".menu");
+//Original Version//
+const questions = [
 
-bars.addEventListener("click", () => {
-    menu.classList.add("active");
-    gsap.from(".menu", {
-        opacity: 0,
-        duration: .3
-    })
 
-    gsap.from(".menu ul", {
-        opacity: 0,
-        x: -300
-    })
+    {
+
+        question: "Siapakah ilmuwan yang dikenal karena perannya dalam memecahkan kode Enigma Jerman selama Perang Dunia II dan kontribusinya dalam pengembangan komputer modern yang digunakan banyak orang-orang saat ini?", //6//
+        answers: [
+            { text: "Charles Babbage", correct: false },
+            { text: "John von Neumann", correct: false },
+            { text: "Grace Hopper", correct: false },
+            { text: " Joseph Oppenheimer", correct: false },
+            { text: "Alan Turing", correct: true },
+        ],
+    },
+    
+    {
+        question: "Apa dampak sosial utama dari penemuan komputer pada abad ke-20?", //1//
+        answers: [
+            { text: "Perubahan pola ekonomi", correct: false },
+            { text: "Perkembangan industri otomotif", correct: false },
+            { text: "Transformasi dalam pendidikan dan pekerjaan", correct: true },
+            { text: "Penurunan minat dalam seni dan budaya", correct: false },
+            { text: "Peningkatan penggunaan obat-obatan terlarang", correct: false },
+        ],
+    },
+
+    {
+        question: "Peristiwa apa yang menandai awal era komputer pribadi dan dampaknya terhadap masyarakat?", //2//
+        answers: [
+            { text: "Penemuan transistor", correct: false },
+            { text: "Peluncuran komputer Apple II", correct: true },
+            { text: "Pembuatan Internet", correct: false },
+            { text: "Terbitnya buku War Games", correct: false },
+            { text: "Pembentukan Perserikatan Bangsa-Bangsa", correct: false },
+        ],
+    },
+    {
+        question: "Bagaimana peran komputer dalam Revolusi Industri 4.0 mempengaruhi lapangan pekerjaan dan sosial?", //3//
+        answers: [
+            { text: "Peningkatan ketergantungan pada teknologi", correct: true },
+            { text: "Peningkatan permintaan pekerja manual", correct: false },
+            { text: "Penurunan angka pengangguran", correct: false },
+            { text: "Kehilangan minat dalam pendidikan tinggi", correct: false },
+            { text: "Meningkatnya pertumbuhan populasi dunia", correct: false },
+        ],
+    },
+
+    {
+        question: "Komputer ENIAC, yang dibangun pada tahun 1940-an, memiliki dampak sosial besar karena:",  //4//
+        answers: [
+            { text: "Memperluas kesenjangan gender di bidang teknologi", correct: true },
+            { text: "Mempercepat pertumbuhan ekonomi global", correct: false },
+            { text: "Mendorong penemuan roket antariksa", correct: false },
+            { text: "Membantu dalam perawatan kesehatan masyarakat", correct: false },
+            { text: "Menyebabkan kemunduran dalam perkembangan transportasi", correct: false },
+        ],
+    },
+
+    {
+        question: "Perkembangan komputer pribadi seperti IBM PC pada tahun 1980-an memberikan akses kepada masyarakat untuk:", //5//
+        answers: [
+            { text: "Terhubung dengan antariksa", correct: false },
+            { text: "Mengakses informasi dan komunikasi pribadi", correct: true },
+            { text: "Menjalankan bisnis besar", correct: false },
+            { text: "Menjadi lebih terisolasi secara sosial", correct: false },
+            { text: "Mempertahankan gaya hidup tradisional", correct: false },
+        ],
+    },
+
+    {
+
+        question: "Apa yang dimaksud dengan hak kekayaan intelektual (HKI) dalam konteks produk informatika?", //6//
+        answers: [
+            { text: "Hak untuk mengakses internet gratis", correct: false },
+            { text: "Hak untuk mengklaim kepemilikan atas karya kreatif", correct: true },
+            { text: "Hak untuk mengekspor perangkat lunak", correct: false },
+            { text: "Hak untuk menghapus data pribadi dari internet", correct: false },
+            { text: "Hak untuk menyalin dan mendistribusikan konten tanpa izin", correct: false },
+        ],
+    },
+
+    {
+
+        question: "Bagaimana produk informatika berkontribusi terhadap pertumbuhan ekonomi?", //7//
+        answers: [
+            { text: "Dengan membatasi akses informasi", correct: false },
+            { text: "Dengan menciptakan lapangan kerja baru", correct: true },
+            { text: "Dengan meningkatkan biaya produksi", correct: false },
+            { text: "Dengan mengurangi investasi dalam teknologi", correct: false },
+            { text: "Dengan meningkatkan kebutuhan akan bahan bakar fosil", correct: false },
+        ],
+    },
+
+    {
+        question: " Manakah dari pernyataan berikut yang benar tentang variabel dalam pemrograman prosedural?", //8//
+        answers: [
+            { text: "Variabel hanya dapat diinisialisasi sekali", correct: false },
+            { text: " Variabel adalah simbol yang digunakan untuk menyimpan dan memanipulasi data", correct: true },
+            { text: "Variabel harus selalu dideklarasikan sebelum digunakan", correct: false },
+            { text: "Variabel hanya dapat menyimpan satu nilai pada satu waktu", correct: false },
+            { text: "Variabel harus selalu dideklarasikan dengan tipe data yang sama", correct: false },
+        ],
+    },
+
+   
+
+    {
+        question: " Apa yang dimaksud dengan prosedur dalam pemrograman prosedural?", //9//
+        answers: [
+            { text: "Sebuah blok kode yang menjalankan operasi aritmatika", correct: false },
+            { text: " ebuah blok kode yang menjalankan serangkaian instruksi secara berurutan", correct: true },
+            { text: "Sebuah blok kode yang menghasilkan nilai balik", correct: false },
+            { text: "Sebuah blok kode yang memungkinkan untuk melakukan percabangan", correct: false },
+            { text: "Sebuah metode untuk membagi program menjadi modul terpisah", correct: false },
+        ],
+    },
+
+
+    {
+        question: "Manakah dari struktur pengendalian berikut yang digunakan untuk melakukan iterasi dalam pemrograman prosedural?", //10//
+        answers: [
+            { text: "If-else", correct: false },
+            { text: " For", correct: true },
+            { text: "Switch", correct: false },
+            { text: "Try-catch", correct: false },
+            { text: "While", correct: false },
+        ],
+    },
+
+
+    
+];
+
+
+
+let currentQuestionIndex = 0;
+let userScore = 0;
+let restartAttempts = 0;
+const maxRestarts = 3;
+
+const startButtonEl = document.querySelector(".start-btn");
+const welcomeScreenEl = document.querySelector(".welcome-screen");
+const quizScreenEl = document.querySelector(".quiz-screen");
+const questionEl = document.querySelector(".question");
+const answersButtons = document.querySelector(".answers-container");
+const nextButtonEl = document.querySelector(".next-btn");
+
+startButtonEl.addEventListener("click", startQuiz);
+
+function startQuiz() {
+    if (restartAttempts < maxRestarts) {
+        welcomeScreenEl.style.display = "none";
+        quizScreenEl.style.display = "flex";
+        currentQuestionIndex = 0;
+        userScore = 0;
+        nextButtonEl.innerHTML = "Next";
+        nextButtonEl.style.display = "none";
+        displayQuestion();
+    } else {
+        alert("You have reached the maximum number of restarts. Please re-authenticate.");
+        authenticateNewPassword();
+    }
+}
+
+function displayQuestion() {
+    resetContainer();
+    questionEl.textContent = questions[currentQuestionIndex].question;
+    questions[currentQuestionIndex].answers.forEach((answer) => {
+        const buttonEl = document.createElement("button");
+        buttonEl.innerHTML = answer.text;
+        buttonEl.classList.add("ans-btn");
+        answersButtons.appendChild(buttonEl);
+
+        if (answer.correct) {
+            buttonEl.dataset.correctAns = answer.correct;
+        }
+
+        buttonEl.addEventListener("click", checkAnswer);
+    });
+}
+
+function checkAnswer(e) {
+    const selectedButton = e.target;
+    if (selectedButton.dataset.correctAns) {
+        userScore++;
+        selectedButton.classList.add("correct-ans");
+    } else {
+        selectedButton.classList.add("wrong-ans");
+    }
+
+    Array.from(answersButtons.children).forEach((button) => {
+        if (button.dataset.correctAns === "true") {
+            button.classList.add("correct-ans");
+        }
+        button.disabled = true;
+    });
+
+    nextButtonEl.style.display = "block";
+}
+
+function displayResult() {
+    resetContainer();
+    questionEl.innerHTML = `Quiz is Completed! <br> Your Score: <span class="score">${userScore}/${questions.length}</span>`;
+    nextButtonEl.innerHTML = "Restart Quiz";
+    restartAttempts++;
+}
+
+function nextQuestion() {
+    currentQuestionIndex++;
+    if (currentQuestionIndex < questions.length) {
+        displayQuestion();
+        nextButtonEl.style.display = "none";
+    } else {
+        displayResult();
+    }
+}
+
+nextButtonEl.addEventListener("click", function () {
+    if (currentQuestionIndex < questions.length) {
+        nextQuestion();
+    } else {
+        startQuiz();
+    }
 });
 
-close.addEventListener("click", () => {
-    menu.classList.remove("active")
-});
-
-function animateContent(selector) {
-    selector.forEach((selector) => {
-        gsap.to(selector, {
-            y: 30,
-            duration: 0.1,
-            opacity: 1,
-            delay: 0.2,
-            stagger: 0.2,
-            ease: "power2.out",
-        });
-    });
+function resetContainer() {
+    questionEl.textContent = "";
+    answersButtons.innerHTML = "";
 }
 
-function scrollTirggerAnimation(triggerSelector, boxSelectors) {
-    const timeline = gsap.timeline({
-        scrollTrigger: {
-            trigger: triggerSelector,
-            start: "top 50%",
-            end: "top 80%",
-            scrub: 1,
-        },
-    });
+// Passwords list
+const passwords = [
+    { first: "s", second: "5" },
+    { first: "m", second: "10" },
+    { first: "d", second: "15" },
+    // Add more passwords as needed
+];
 
-    boxSelectors.forEach((boxSelector) => {
-        timeline.to(boxSelector, {
-            y: 0,
-            duration: 1,
-            opacity: 1,
-        });
-    })
+function getRandomPasswordSet() {
+    const randomIndex = Math.floor(Math.random() * passwords.length);
+    return passwords[randomIndex];
 }
 
-function swipeAnimation(triggerSelector, boxSelectors) {
-    const timeline = gsap.timeline({
-        scrollTrigger: {
-            trigger: triggerSelector,
-            start: "top 50%",
-            end: "top 100%",
-            scrub: 3,
-        },
-    });
+function authenticate() {
+    let passwordAttempts = 3;
+    const delayTime = 3000;
+    let firstPasswordEntered = false;
+    let secondPasswordEntered = false;
+    const { first: firstPassword, second: secondPassword } = getRandomPasswordSet();
 
-    boxSelectors.forEach((boxSelector) => {
-        timeline.to(boxSelector, {
-            x: 0,
-            duration: 1,
-            opacity:1,
-        });
-    });
+    while (passwordAttempts > 0) {
+        if (!firstPasswordEntered) {
+            const inputPassword = prompt("Masukkan kata sandi pertama:");
+
+            if (inputPassword === firstPassword) {
+                firstPasswordEntered = true;
+                alert("Kata sandi pertama benar. Masukkan kata sandi kedua.");
+            } else {
+                passwordAttempts--;
+                alert("Kata sandi pertama salah. Sisa percobaan: " + passwordAttempts);
+
+                if (passwordAttempts === 0) {
+                    alert("Anda telah mencapai jumlah maksimum percobaan. Silakan coba lagi dalam beberapa detik.");
+                    setTimeout(authenticate, delayTime);
+                    return;
+                }
+            }
+        } else if (!secondPasswordEntered) {
+            const inputPassword = prompt("Masukkan kata sandi kedua:");
+
+            if (inputPassword === secondPassword) {
+                secondPasswordEntered = true;
+                unlockWebsite();
+                return;
+            } else {
+                passwordAttempts--;
+                alert("Kata sandi kedua salah. Sisa percobaan: " + passwordAttempts);
+
+                if (passwordAttempts === 0) {
+                    alert("Anda telah mencapai jumlah maksimum percobaan. Silakan coba lagi dalam beberapa detik.");
+                    setTimeout(authenticate, delayTime);
+                    return;
+                }
+            }
+        }
+    }
 }
 
-function galleryAnimation(triggerSelector, boxSelectors) {
-    const timeline = gsap.timeline({
-        scrollTrigger: {
-            trigger: triggerSelector,
-            start: "top 250%",
-            end: "bottom 250%",
-            scrub: 1,
-        },
-    });
+function authenticateNewPassword() {
+    let passwordAttempts = 3;
+    const delayTime = 3000;
+    let firstPasswordEntered = false;
+    let secondPasswordEntered = false;
+    let thirdPasswordEntered = false;
 
-    boxSelectors.forEach((boxSelector) => {
-        timeline.to(boxSelector, {
-            y: 0,
-            opacity: 1,
-            duration: 1,
-        });
-    });
+    const newPasswords = [
+        { 
+        first: "newPassword1", 
+        second: "newPassword2", 
+        third: "newPassword3" },
+        // Add more password sets as needed
+    ];
+
+    const { first: firstPassword, second: secondPassword, third: thirdPassword } = newPasswords[0];
+
+    while (passwordAttempts > 0) {
+        if (!firstPasswordEntered) {
+            const inputPassword = prompt("Masukkan kata sandi baru pertama:");
+
+            if (inputPassword === firstPassword) {
+                firstPasswordEntered = true;
+                alert("Kata sandi pertama benar. Masukkan kata sandi kedua.");
+            } else {
+                passwordAttempts--;
+                alert("Kata sandi pertama salah. Sisa percobaan: " + passwordAttempts);
+
+                if (passwordAttempts === 0) {
+                    alert("Anda telah mencapai jumlah maksimum percobaan. Silakan coba lagi dalam beberapa detik.");
+                    setTimeout(authenticateNewPassword, delayTime);
+                    return;
+                }
+            }
+        } else if (!secondPasswordEntered) {
+            const inputPassword = prompt("Masukkan kata sandi baru kedua:");
+
+            if (inputPassword === secondPassword) {
+                secondPasswordEntered = true;
+                alert("Kata sandi kedua benar. Masukkan kata sandi ketiga.");
+            } else {
+                passwordAttempts--;
+                alert("Kata sandi kedua salah. Sisa percobaan: " + passwordAttempts);
+
+                if (passwordAttempts === 0) {
+                    alert("Anda telah mencapai jumlah maksimum percobaan. Silakan coba lagi dalam beberapa detik.");
+                    setTimeout(authenticateNewPassword, delayTime);
+                    return;
+                }
+            }
+        } else if (!thirdPasswordEntered) {
+            const inputPassword = prompt("Masukkan kata sandi baru ketiga:");
+
+            if (inputPassword === thirdPassword) {
+                thirdPasswordEntered = true;
+                unlockWebsite();
+                return;
+            } else {
+                passwordAttempts--;
+                alert("Kata sandi ketiga salah. Sisa percobaan: " + passwordAttempts);
+
+                if (passwordAttempts === 0) {
+                    alert("Anda telah mencapai jumlah maksimum percobaan. Silakan coba lagi dalam beberapa detik.");
+                    setTimeout(authenticateNewPassword, delayTime);
+                    return;
+                }
+            }
+        }
+    }
 }
 
-
-
-
-animateContent([".home .content h5, .home .content h1, .home .content p, .home .content .search"]);
-
-scrollTirggerAnimation(".travel", [".travel .box1", ".travel .box2", ".travel .box3"]);
-
-scrollTirggerAnimation(".feedback .container", [".feedback .label", ".feedback .heading", ".feedback .paragraph"]);
-
-scrollTirggerAnimation(".article", [".article .label", ".article .heading"]);
-
-swipeAnimation(".destinations", [".destinations .heading", ".destinations .content"])
-
-swipeAnimation(".article", [".article .latest-article", ".article .box1", ".article .box2", ".article .box3", ".article .box4"])
-
-galleryAnimation(".destinations .gallery", [".destinations .gallery .box1",".destinations .gallery .box2",".destinations .gallery .box3",".destinations .gallery .box4",".destinations .gallery .box5"])
-
-galleryAnimation(".featured .gallery", [".featured .gallery .box1",".featured .gallery .box2",".featured .gallery .box3",".featured .gallery .box4"])
-
-galleryAnimation(".feedback .voices", [".feedback .voices .box1",".feedback .voices .box2",".feedback .voices .box3",".feedback .voices .box4",".feedback .voices .box5",".feedback .voices .box6"])
-
-
-
-
-
-
+function unlockWebsite() {
+    document.body.classList.remove("locked");
+    restartAttempts = 0;
+}
 
 document.addEventListener("DOMContentLoaded", function() {
-    // Mendapatkan semua elemen gambar di dalam kelas 'team-member'
-    const teamMemberImages = document.querySelectorAll('.team-member img');
-
-    // Menambahkan event listener untuk setiap gambar
-    teamMemberImages.forEach(image => {
-        // Menangkap saat kursor diarahkan ke gambar
-        image.addEventListener('mouseenter', function() {
-            // Menggerakkan gambar ke kiri
-            this.style.transform = 'translateX(-20px)';
-        });
-
-        // Menangkap saat kursor tidak lagi diarahkan ke gambar
-        image.addEventListener('mouseleave', function() {
-            // Mengembalikan gambar ke posisi awal
-            this.style.transform = 'translateX(0)';
-        });
-    });
+    authenticate();
 });
+
+
+
+
 
 
 
